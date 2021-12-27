0d08c31db635dc545b0ccf58a928cd79be2208b7
tssule3 <tssule3@gmail.com>
2021-12-27 12:12:24 +0530

    Added changes for firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // comment
   isMenuCollapsed = true;
   images = ['../assets/slider-1.jpg','../assets/slider-2.jpg','../assets/slider-3.jpg'];
 }
