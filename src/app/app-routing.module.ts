09e07c382f9e09c3642c0c2461b7a797235aa3c8
tssule3 <tssule3@gmail.com>
2021-12-27 22:12:55 +0530

    Added changes for login register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticationModule } from './authentication/authentication.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: 'authentication', loadChildren: () => import('./authentication/authentication.module').then(mod => AuthenticationModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
